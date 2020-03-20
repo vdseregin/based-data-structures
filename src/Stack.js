b68f4067adf0ed6d6a4cb8e3e9ddebfa57860d95
Vladislav Seregin <vd.seregin@gmail.com>
2020-03-20 08:27:28 +0300

    Add implementation of Stack

---
+++ src/Stack.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null
+  }
+}
+
+
+class Stack {
+  constructor(value) {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.size) {
+      this.first = newNode;
+      this.last = this.first;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+
+    this.size++;
+    return this.size;
+  }
+
+  pop() {
+    if (!this.size) {
+      return null;
+    }
+    let temp = this.first;
+
+    if (this.size === 1) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
